73100737d41615896ff97822cc0b134d180ac31a
marielsa-design <marielsa.milagro@gmail.com>
2026-03-16 16:04:24 -0500

    Inventario-Riwi

---
+++ Ejerciciofuncional03.py
@@ -0,0 +1,31 @@
+# Sistema simple de cálculo de salario
+
+# Una empresa paga a sus empleados según las horas trabajadas.
+
+# Debes crear una función llamada:
+
+# calcular_salario(horas, valor_hora)
+
+# Reglas:
+
+# Si trabaja hasta 40 horas, se paga normal
+
+# Si trabaja más de 40 horas, las horas extra se pagan al doble
+
+# La función debe:
+
+# Calcular el salario total
+
+# Retornar el resultado
+
+# El programa principal debe:
+
+# Pedir el nombre del empleado
+
+# Pedir horas trabajadas
+
+# Pedir valor por hora
+
+# Llamar a la función
+
+# Mostrar el salario final.
