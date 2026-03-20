c4c831fc746716695d4aefeb9f46f157734c0cf0
marielsa-design <mmarielsa.milagro@gmail.com>
2026-03-20 15:29:53 -0500

    Inventario-Riwi

--- main2.py
+++ main2.py
@@ -1,64 +1,38 @@
 inventario = []
 
-def agregar_producto(inventario):
-    nombre = input("Ingrese el nombre del producto: ")
-    
-    # Validar el precio
-    while True:
-        try:
-            precio = float(input("Ingrese el precio de precio: "))
-            break
-        except ValueError:
-            print("Error. Ingrese un numero valido")
+# def agregar_producto(inventario):
+#     nombre = input("Ingrese el nombre del producto: ")
 
-    #Validar la cantidad de precio
-    while True:
-        try:
-            cantidad = int(input("Ingrese la cantidad de precio: "))
-            break
-        except ValueError:
-            print("Error. Ingresar un numero enterio valido")
+#     # Validar el precio
+#     while True:
+#         try:
+#             precio = float(input("Ingrese el precio de precio: "))
+#             break
+#         except ValueError:
+#             print("Error. Ingrese un numero valido")
 
-    producto = {
-        "nombre": nombre,
-        "precio": precio,
-        "cantidad": cantidad
-    }
+#     #Validar la cantidad de precio
+#     while True:
+#         try:
+#             cantidad = int(input("Ingrese la cantidad de precio: "))
+#             break
+#         except ValueError:
+#             print("Error. Ingresar un numero enterio valido")
 
-    inventario.append(producto)
-    print(f"Producto agregado correctamente")
+#     producto = {
+#         "nombre": nombre,
+#         "precio": precio,
+#         "cantidad": cantidad
+#     }
 
-# Agregar a la lista de producto
-
-def mostrar_inventario(inventario):
-    if len(inventario) == 0:
-        print(f"El inventario esta vacio")
-    else:
-        print(f"Inventario: ")
-        for producto in inventario:
-            print(f"Producto: {producto['nombre']} | Precio: {producto['precio']} | Cantidad: {producto['cantidad']}")
-
-# Calcular de estadisticas de producto
-
-def calcular_estadisticas(inventario):
-    if len(inventario) == 0:
-        print("No hay productos para calcular")
-    else:
-        total_valor = 0
-        total_producto = 0
-        for producto in inventario:
-            total_valor += producto["precio"] * producto["cantidad"]
-            total_producto += producto["cantidad"]
-        
-        print("Estadísticas:")
-        print(f"Valor total del inventario: {total_valor}")
-        print(f"Cantidad total de productos: {total_producto}")
+#     inventario.append(producto)
+#     print(f"Producto agregado correctamente")
 
 def menu():
     cupos = 5
     inventario = ['vacio'] * cupos
 
-    opcion = 1
+    opcion = 0
     print("Bienvenido al sistema de inventario")
     while opcion < 4:
         print("1. Agregar producto")
@@ -80,40 +54,30 @@ def menu():
                 else:
                     print("Inventario lleno!!")
             case 2:
-                def mostrar_inventario():
-                    for i, carro in enumerate(inventario, start=1):
-                        print(f"{i}. {producto}")
-                if len(inventario) > 0:
-                    producto = input("Ingresa el producto\n").upper()
-                    if producto in inventario:
-                        inventario.remove(producto)
-                        print(f"producto retirado: {producto}")
+                def mostrar_inventario(inventario):
+                    if len(inventario) == 0:
+                        print(f"El inventario esta vacio")
                     else:
-                        print(f"{producto} no esta ingresado")
-                else:
-                    print("Inventario lleno!!")
+                        print(f"Inventario: ")
+                    for producto in inventario:
+                        print(f"Producto: {producto['nombre']} | Precio: {producto['precio']} | Cantidad: {producto['cantidad']}")
             case 3:
-                print(inventario)
-            case _:
+                def calcular_estadisticas(inventario):
+                    if len(inventario) == 0:
+                        print("No hay productos para calcular")
+                    else:
+                        total_valor = 0
+                        total_producto = 0
+                    for producto in inventario:
+                        total_valor += producto["precio"] * producto["cantidad"]
+                        total_producto += producto["cantidad"]
+
+                    print("Estadísticas:")
+                    print(f"Valor total del inventario: {total_valor}")
+                    print(f"Cantidad total de productos: {total_producto}")
+            case 4:
                 print("Adios!!")
                 break
-
-        # opcion = input("Seleccione una opción: ")
-        # if opcion == "1":
-        #     agregar_producto(inventario)
-
-        # elif opcion == "2":
-        #     mostrar_inventario(inventario)
-
-        # elif opcion == "3":
-        #     calcular_estadisticas(inventario)
-
-        # elif opcion == "4":
-        #     print("Adios!")
-        #     break
-
-        # else:
-        #     print("Opcion invalida, intente de nuevo")
 
 menu()
 
